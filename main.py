02f8ca1b36a519f87807dbd8ac200353882ad4d5
Andreas <andreasbalevik@gmail.com>
2016-09-07 17:02:25 +0200

    facebook update login
    fixed facebook login redirect to home and show as logged in

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def facebook_authorized(resp):
         )
     session['oauth_token'] = (resp['access_token'], '')
     me = facebook.get('/me')
-    return 'Logged in as id=%s name=%s redirect=%s' % \
-        (me.data['id'], me.data['name'], request.args.get('next'))
+    return render_template("home.html", value='Logged in as %s' % \
+        (me.data['name']))
 
 
 @facebook.tokengetter
